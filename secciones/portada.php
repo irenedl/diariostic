796c96909a6eae5afa6baed5d08b5cbacf086824
Irene Domínguez Lafuente <55579432+irenedl@users.noreply.github.com>
2021-10-30 10:20:06 +0200

    Update portada.php

--- secciones/portada.php
+++ secciones/portada.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "La portada",
+"titulo" => "Portada actualizada",
 "autor" => "mtorres",
 "resumen" => "El resumen",
 ];
